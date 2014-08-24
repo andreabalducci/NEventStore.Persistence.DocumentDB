3c223efe761329427e3c800059598b123153fe6c
andreabalducci <mtb.snowboard@gmail.com>
2014-08-24 15:08:23 +0200

    project setup

---
+++ Playground/DocumentDBSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    public static class DocumentDbSettings
+    {
+        public static string AuthorizationKey {
+            get { return
+                Environment.GetEnvironmentVariable("DocumentDB_PrimaryKey"); 
+            }
+        }
+
+        public static Uri EndPoint
+        {
+            get { return new Uri(EndPointString); }
+        }
+
+        private static string EndPointString {
+            get { return
+                Environment.GetEnvironmentVariable("DocumentDB_EndPoint"); }
+        }      
+
+        public static string DatabaseId {
+            get { return "nes-playground"; }
+        }
+    }
+}
---
+++ Playground/when_creating_a_new_database.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Machine.Specifications;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+
+namespace Playground
+{
+    // ReSharper disable InconsistentNaming
+    [Subject("Server")]
+    public class with_new_server
+    {
+        private static DocumentClient client;
+        private static Database _database;
+        private static ResourceResponse<Database> response;
+
+        private Establish context = () =>
+        {
+            client = new DocumentClient(DocumentDbSettings.EndPoint, DocumentDbSettings.AuthorizationKey);
+            _database = new Database {Id = DocumentDbSettings.DatabaseId};
+        };
+
+        private It database_id_should_match = () => response.Resource.Id.ShouldBeLike(_database.Id);
+        private Because of = () => { response = client.CreateDatabaseAsync(_database).Await(); };
+
+        private Cleanup resources = () =>
+        {
+            Database database = client
+                .CreateDatabaseQuery()
+                .Where(db => db.Id == DocumentDbSettings.DatabaseId)
+                .AsEnumerable().FirstOrDefault();
+
+            if (database != null)
+            {
+                client.DeleteDatabaseAsync(database.SelfLink).Await();
+            }
+
+            client.Dispose();
+        };
+    }
+}
